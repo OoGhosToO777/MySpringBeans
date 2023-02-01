f1b1802f0c95997b9ff67f99382302ff9bebd868
Anatoly Korbasov <anatolykorbasov@gmail.com>
2023-02-01 22:08:11 +0300

    Test passed

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -18,7 +18,11 @@ public class AnimalsCage {
         System.out.println("Say:");
         System.out.println(animal.toString());
         System.out.println("At:");
-        System.out.println(timer.getTime());
+        System.out.println(getTimer().getTime());
         System.out.println("________________________");
+    }
+
+    public Timer getTimer() {
+        return timer;
     }
 }
